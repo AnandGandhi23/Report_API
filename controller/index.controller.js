4b14debd0c8250a00620635e930334789e388ac4
Anand Gandhi <anandgandhi19@gmail.com>
2022-01-11 00:26:16 +0530

    cogs query changed

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -14,7 +14,7 @@ const getReportData = (req, res) => {
         "SELECT SUM(NetSales) as returnSale FROM sales_actual WHERE TransactionType='Returned' AND TransactionDate NOT REGEXP '0000-00-00' AND (TransactionDate BETWEEN '" + startDate + "' AND '" + endDate + "');" +
         "SELECT SUM(NetSales) as cancelIncome FROM sales_actual WHERE TransactionType='Cancelled' AND TransactionDate NOT REGEXP '0000-00-00' AND (TransactionDate BETWEEN '" + startDate + "' AND '" + endDate + "');" +
         "SELECT SUM(NetSales) as totalIncome FROM sales_actual WHERE TransactionDate NOT REGEXP '0000-00-00' AND (TransactionDate BETWEEN '" + startDate + "' AND '" + endDate + "');" +
-        "SELECT SUM(Expenses_Amount) as cogs FROM cogs WHERE 'Transaction Date' NOT REGEXP '0000-00-00' AND ('Transaction Date' BETWEEN '" + startDate + "' AND '" + endDate + "');" +
+        "SELECT SUM(Expenses_Amount) as cogs FROM cogs WHERE `Transaction Date` NOT REGEXP '0000-00-00' AND (`Transaction Date` BETWEEN '" + startDate + "' AND '" + endDate + "');" +
         "SELECT SUM(commission_due) as commissionConsultant FROM commissions WHERE sale_date NOT REGEXP '0000-00-00' AND (sale_date BETWEEN '" + startDate + "' AND '" + endDate + "');" +
         "SELECT SUM(commission_due) as commissionPCC FROM `non-hcp_commissions` WHERE sale_date NOT REGEXP '0000-00-00' AND (sale_date BETWEEN '" + startDate + "' AND '" + endDate + "');" +
         "SELECT SUM(commission_due) as commissionTelemarketing FROM `non-hcp_commissions` WHERE role='Telemarketing' AND sale_date NOT REGEXP '0000-00-00' AND (sale_date BETWEEN '" + startDate + "' AND '" + endDate + "')"
@@ -41,7 +41,7 @@ const getReportDataByYearr = (req, res) => {
         "SELECT SUM(NetSales) as returnSale FROM sales_actual WHERE TransactionType='Returned' AND year(TransactionDate)=" + year + ";" +
         "SELECT SUM(NetSales) as cancelIncome FROM sales_actual WHERE TransactionType='Cancelled' AND year(TransactionDate)=" + year + ";" +
         "SELECT SUM(NetSales) as totalIncome FROM sales_actual WHERE year(TransactionDate)=" + year + ";" +
-        "SELECT SUM(Expenses_Amount) as cogs FROM cogs WHERE year('Transaction Date')=" + year + ";" +
+        "SELECT SUM(Expenses_Amount) as cogs FROM cogs WHERE year(`Transaction Date`)=" + year + ";" +
         "SELECT SUM(commission_due) as commissionConsultant FROM commissions WHERE year(sale_date)=" + year + ";" +
         "SELECT SUM(commission_due) as commissionPCC FROM `non-hcp_commissions` WHERE year(sale_date)=" + year + ";" +
         "SELECT SUM(commission_due) as commissionTelemarketing FROM `non-hcp_commissions` WHERE role='Telemarketing' AND year(sale_date)=" + year
