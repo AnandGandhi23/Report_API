a15a16a39f2c64b25ceaa4b3a432fcefc2636c0f
Anand Gandhi <anandgandhi19@gmail.com>
2022-01-28 23:58:24 +0530

    removed emoty location group

--- controller/franchise-location.controller.js
+++ controller/franchise-location.controller.js
@@ -28,7 +28,8 @@ const getDistinctLocationGroup = (req, res) => {
         }
 
         const sqlQuery =
-        "SELECT DISTINCT `location group` as label, location_id as value FROM `franchise_locations` WHERE franchise_id NOT IN('CF0034', 'CF0037', 'CF0049') order by `location group`;";
+        "SELECT DISTINCT `location group` as label, location_id as value FROM `franchise_locations` WHERE franchise_id NOT IN('CF0034', 'CF0037', 'CF0049') " + 
+        "AND `location group` IS NOT NULL AND `location group` <> '' order by `location group`;";
         connection.query(sqlQuery, function(err, results) {
             connection.release();
             if (!err) {
